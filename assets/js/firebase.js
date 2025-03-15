5d2bd704a0b6ea03f1dbc1a06b44930a234cd467
Labh Bothra <labhbothra1380@gmail.com>
2025-03-15 08:40:55 +0530

    Check Report Download

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -7,19 +7,13 @@ import { getStorage } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-s
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
-  apiKey: "",
-
-  authDomain: "",
-
-  projectId: "",
-
-  storageBucket: "",
-
-  messagingSenderId: "",
-
-  appId: "",
-
-  measurementId: "",
+  apiKey: "AIzaSyDHV3k40GpvR4ebqEbLuBlPudYjlx4p0rU",
+  authDomain: "react-native-a54a3.firebaseapp.com",
+  projectId: "react-native-a54a3",
+  storageBucket: "react-native-a54a3.firebasestorage.app",
+  messagingSenderId: "516700173524",
+  appId: "1:516700173524:web:5d927f9b98cb0b2fc17741",
+  measurementId: "G-NLHDM2D462"
 };
 
 const app = initializeApp(firebaseConfig);
